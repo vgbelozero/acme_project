69021c33f886e249f41194a13dd8b6829ca2489f
VBelozerov <vgbelozero@yandex.ru>
2026-06-13 18:00:31 +0500

    init

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,14 @@
+# birthday/forms.py
+from django import forms
+
+
+class BirthdayForm(forms.Form):
+    first_name = forms.CharField(label='Имя', max_length=20)
+    last_name = forms.CharField(
+        label='Фамилия', required=False, help_text='Необязательное поле'
+    )
+    birthday = forms.DateField(
+        label='Дата рождения',
+        # Указываем, что виджет для ввода даты должен быть с типом date.
+        widget=forms.DateInput(attrs={'type': 'date'})
+    ) 
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,14 @@
+# birthday/views.py
 from django.shortcuts import render
+
+# Импортируем класс BirthdayForm, чтобы создать экземпляр формы.
+from .forms import BirthdayForm
 
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+    # Создаём экземпляр класса формы.
+    form = BirthdayForm()
+    # Добавляем его в словарь контекста под ключом form:
+    context = {'form': form}
+    # Указываем нужный шаблон и передаём в него словарь контекста.
+    return render(request, 'birthday/birthday.html', context)
